d9224119b202e445ecd7ac760fd5584655912312
TA1128 <m.khaidar.ft21@mail.umy.ac.id>
2022-10-26 16:20:55 +0700

    menambahkan function while

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -10,4 +10,23 @@ namespace SearchAlgorithm
         int n;
         //Get the number of elements to store in the array
         int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("n\Array should have minimum 1 and maximum 20 element. \n");
+            } 
+
+
+        
+
+       
+
     
