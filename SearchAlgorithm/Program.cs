04da17df38c7ba37a3361ee0a5b2b2a58b2bfaa4
TA1128 <m.khaidar.ft21@mail.umy.ac.id>
2022-10-26 17:32:27 +0700

    menambahkan case 1, 2,, 3

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -113,6 +113,32 @@ namespace SearchAlgorithm
                 Console.WriteLine("3. Exit");
                 Console.WriteLine("Enter your choice (1,2,3) : ");
                 pilihanmenu = Convert.ToInt32(Console.ReadLine());
+                switch(pilihanmenu)
+                {
+                    case 1:
+                        Console.WriteLine("");
+                        Console.WriteLine(".......................");
+                        Console.WriteLine("Linear Search");
+                        Console.WriteLine(".......................");
+                        MyList.input();
+                        MyList.LinearSearch();
+                        break;
+                    case 2:
+                        Console.WriteLine("");
+                        Console.WriteLine(".......................");
+                        Console.WriteLine("Linear Search");
+                        Console.WriteLine(".......................");
+                        MyList.input();
+                        MyList.LinearSearch();
+                        break;
+                    case 3:
+                        Console.WriteLine("exit");
+                        break;
+                    default:
+                        Console.WriteLine("Error");
+                        break;
+
+                }
             }
         }
     }
